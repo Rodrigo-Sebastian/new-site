ef4fdfc630c3744246aec63e7844070ec4d55145
Rebecka <rebeckagamble@hotmail.com>
2024-02-26 11:15:47 +0100

    search in nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,15 +12,13 @@ function Navbar() {
             Chas News
           </h2>
         </Link>
-        <div className="py-2 mx-2 flex flex-row ">
-        <div className="pr-6">
-            <SearchBar />
-          </div>
+        <div className="py-2 mx-2 ">
           <Link href="/bookmarkedNews">
             <FaRegBookmark size={20} />
           </Link>
         </div>
       </div>
+      <div className="flex flex-row bg-[#4C64FF] items-center justify-between">
       <nav className="bg-[#4C64FF] flex flex-row h-10 items-center w-full">
         <ul className="list-none flex flex-row gap-4 sm:gap-6 xl:w-full mx-2 sm:mx-6 xl:max-w-[80%] 2xl:max-w-[50%] xl:mx-auto">
           <li className="">
@@ -40,6 +38,10 @@ function Navbar() {
           </li>
         </ul>
       </nav>
+      <div className="px-6">
+            <SearchBar />
+          </div>
+          </div>
     </header>
   );
 }
